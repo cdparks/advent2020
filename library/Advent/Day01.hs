5b60789f0a1bfe4f25d3c668293071f8c7bd9f9d
Chris Parks <christopher.daniel.parks@gmail.com>
2020-12-01 10:26:51 -0800

    Add solution for day 1

--- library/Advent/Day01.hs
+++ library/Advent/Day01.hs
@@ -5,5 +5,32 @@ where
 
 import Advent.Prelude
 
+import qualified Data.HashSet as HashSet
+import qualified Data.Text.IO as Text
+
 main :: Part -> IO ()
-main _part = pure ()
+main part = do
+  entries <- toEntries <$> Text.getContents
+  let result = listToMaybe $ findEntries 2020 entries
+  maybe (die "no valid set") (print . product) result
+ where
+  findEntries = case part of
+    Part1 -> pairSumsTo
+    Part2 -> tripleSumsTo
+
+toEntries :: Text -> HashSet Int
+toEntries contents = HashSet.fromList $ do
+  line <- lines contents
+  maybeToList $ readMaybe @Int $ unpack line
+
+pairSumsTo :: Int -> HashSet Int -> [[Int]]
+pairSumsTo total entries = do
+  entry <- HashSet.toList entries
+  let other = total - entry
+  guard $ other `HashSet.member` entries
+  pure [entry, other]
+
+tripleSumsTo :: Int -> HashSet Int -> [[Int]]
+tripleSumsTo total entries = do
+  entry <- HashSet.toList entries
+  (entry :) <$> pairSumsTo (total - entry) (HashSet.delete entry entries)
