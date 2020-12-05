281b908dfe3f83a8fe47a1ab66d31aee1a80611a
Christopher Parks <christopher.daniel.parks@gmail.com>
2020-12-05 14:22:51 -0800

    Bit-twiddly version of day 5 solution
    Each seat specifier is really a pair of binary numbers where B/R
    indicate a one bit, and L/F indicate a zero bit. For example:
    
      FBFBBFFRLR
    
    is
    
      FBFBBFF -> 00101100 -> 44
          RLR -> 00000101 ->  5
    
    so the seat id is
    
      8 * 44 + 5 = 357

--- library/Advent/Day05.hs
+++ library/Advent/Day05.hs
@@ -1,33 +1,18 @@
-{-# LANGUAGE StrictData #-}
-
-module Advent.Day05
-  ( main
-  ) where
+module Advent.Day05 where
 
 import Advent.Prelude
 
+import Data.Bits (shift, (.|.))
+import Data.Word (Word8)
+
 main :: Part -> IO ()
 main part = do
-  passes <- toPasses <$> getContents
-  let seatId = pick $ uncurry toSeatId . findSeat <$> passes
+  seatId <- pick . fmap (uncurry toId . toSeat) . lines <$> getContents
   maybe (die "no seat found") print seatId
  where
   pick = case part of
     Part1 -> maximumMay
     Part2 -> missingSeat
-
-toPasses :: Text -> [Pass]
-toPasses = fmap (bimap parse parse . splitAt 7 . unpack) . lines
- where
-  parse = mapMaybe $ \case
-    'F' -> pure Lo
-    'B' -> pure Hi
-    'L' -> pure Lo
-    'R' -> pure Hi
-    _ -> Nothing
-
-findSeat :: Pass -> (Int, Int)
-findSeat = bimap (search 127) (search 7)
 
 missingSeat :: [Int] -> Maybe Int
 missingSeat = headMay . mapMaybe (uncurry skipped) . pairs . sort
@@ -37,27 +22,18 @@ missingSeat = headMay . mapMaybe (uncurry skipped) . pairs . sort
     guard $ after - before == 2
     pure $ before + 1
 
-search :: Int -> [Half] -> Int
-search x = done . foldl' step (start x)
- where
-  start = State min 0
-  done State {..} = pick lo hi
-  step !State {..} = \case
-    Lo -> State min lo (hi - half lo hi)
-    Hi -> State max (lo + half lo hi) hi
+toId :: Int -> Int -> Int
+toId row col = row * 8 + col
 
-half :: Int -> Int -> Int
-half lo hi = (hi - lo + 1) `div` 2
+toSeat :: Text -> (Int, Int)
+toSeat = bimap toInt toInt . splitAt 7 . unpack
+  where toInt = fromBits . toBits
 
-toSeatId :: Int -> Int -> Int
-toSeatId row col = 8 * row + col
+toBits :: String -> [Word8]
+toBits = fmap $ \case
+  'R' -> 1
+  'B' -> 1
+  _ -> 0
 
-type Pass = ([Half], [Half])
-
-data Half = Hi | Lo
-
-data State = State
-  { pick :: Int -> Int -> Int
-  , lo :: Int
-  , hi :: Int
-  }
+fromBits :: [Word8] -> Int
+fromBits = foldl' step 0 where step !n bit = shift n 1 .|. fromIntegral bit
