f7394a54f79b569141921fb28da57c72cfa5e1d9
Christopher Parks <christopher.daniel.parks@gmail.com>
2020-12-06 11:36:31 -0800

    Lift bitwise fold out of parseGroup for day 6

--- library/Advent/Day06.hs
+++ library/Advent/Day06.hs
@@ -2,23 +2,23 @@ module Advent.Day06
   ( main
   ) where
 
-import Advent.Prelude hiding (fold)
+import Advent.Prelude
 
 import Advent.Bits
 import Advent.Parse
 
 main :: Part -> IO ()
 main part = do
-  groups <- parseOrDie $ parseGroups fold
-  print $ sum $ popCount <$> groups
+  groups <- parseOrDie parseGroups
+  print $ sum $ ones <$> groups
  where
-  fold = case part of
+  ones = popCount . case part of
     Part1 -> bitor
     Part2 -> bitand
 
-parseGroups :: ([Word] -> Word) -> Parser [Word]
-parseGroups fold = parseGroup `sepBy1` twoNewlines
-  where parseGroup = fold <$> parseLine `sepBy1` oneSpace
+parseGroups :: Parser [[Word]]
+parseGroups = parseGroup `sepBy1` twoNewlines
+  where parseGroup = parseLine `sepBy1` oneSpace
 
 parseLine :: Parser Word
 parseLine = toWord <$> takeWhile1 isAsciiLower
